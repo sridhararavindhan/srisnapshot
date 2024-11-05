b10f661b85ea6ee70d9e444400f7fff32028797b
sridhararavindhan <sridhar99aravindhan@gmail.com>
2024-11-05 23:38:41 +0530

    Updates

---
+++ precache-manifest.5f8b130c1810afba741cfa3f98179db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243bfc1a8397c586fca34afcc0691a01",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "ccad3e1555664c1a80e0",
+    "url": "/SnapShot/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "e05acad70ec806ca15fa",
+    "url": "/SnapShot/static/js/2.7eef52eb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SnapShot/static/js/2.7eef52eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccad3e1555664c1a80e0",
+    "url": "/SnapShot/static/js/main.22a4ed23.chunk.js"
+  },
+  {
+    "revision": "5d8ea8bbc63e642a234c",
+    "url": "/SnapShot/static/js/runtime-main.7774f14f.js"
+  }
+]);
